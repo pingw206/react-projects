38e038932f46e1ceb0d688dc46f1dc49fc84688c
wenping <pingw206@gmail.com>
2020-02-16 11:44:13 +0800

    first version

---
+++ poketimes/src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>lorem  dedego deog deg iw de wodr edego deog deg iw de wo</p>
+    </div>
+  )
+}
+
+export default About;
---
+++ poketimes/src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>lorem  dedego deog deg iw de wodr edego deog deg iw de wo</p>
+    </div>
+  )
+}
+
+export default Home;
