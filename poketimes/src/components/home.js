c3047ac27171d761773795a1fb7321d216e4dc1a
wenping <pingw206@gmail.com>
2020-03-02 21:44:33 +0800

    exchange data source with redux store

--- poketimes/src/components/home.js
+++ poketimes/src/components/home.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import pokeball from '../pokeball.png';
-class Home extends Component {
-  state = {
-    posts: [ ]
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => {
-        this.setState({
-          posts: res.data.slice(0,10)
-        })
-      })
-  }
+import {connect} from 'react-redux'
 
+
+class Home extends Component {
   render(){
-    const {posts} = this.state;
+    console.log(this.props)
+    const {posts} = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return(
@@ -43,5 +34,10 @@ class Home extends Component {
   }
 }
 
+const mapStateToprops = (state) =>{
+  return {
+    posts: state.posts
+  }
+}
 
-export default Home;
+export default connect(mapStateToprops)(Home);
--- poketimes/src/components/post.js
+++ poketimes/src/components/post.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { deletePost } from '../actions/postActions'
+
 class Post extends Component {
-  state = {
-    post: null
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/')
   }
-  componentDidMount(){
-    let id = this.props.match.params.post_id;
-    axios.get('https://jsonplaceholder.typicode.com/posts/'+ id)
-      .then(res => {
-        this.setState({
-          post: res.data
-        });
-      });
-  }
+
   render(){
-    const post = this.state.post ? (
+    console.log(this.props);
+    const post = this.props.post ? (
       <div className='post'>
-        <h4 className='center'>{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className='center'>{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
+          <div className='center'>
+            <button className='btn grey' onClick={this.handleClick}>
+              Delete Post
+            </button>
+          </div>
       </div>
     ):(
       <div className='center'>Loading post...</div>
@@ -30,4 +31,17 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find((post) => { return post.id === id})
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => { dispatch(deletePost(id))}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
