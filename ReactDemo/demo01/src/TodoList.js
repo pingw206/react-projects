8b01d1469632f7397f630c381a6ba7f8a7f0a5a6
pingw206 <pingw206@gmail.com>
2020-05-11 22:06:34 +0800

    拆分UI组件，改为无状态组件

--- ReactDemo/demo01/src/TodoList.js
+++ ReactDemo/demo01/src/TodoList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import {Input, Button, List} from 'antd';
-import store from './store/index'
-
+import store from './store/index';
+import { changeInputAction,addItemAction,deleteItemAction } from './store/actionCreators';
+import TodoListUI from './TodoListUI';
 
 class TodoList extends Component {
     constructor(props){
@@ -11,51 +10,34 @@ class TodoList extends Component {
         this.changeInputValue = this.changeInputValue.bind(this);
         this.storeChange = this.storeChange.bind(this);
         this.clickBtn = this.clickBtn.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
         store.subscribe(this.storeChange); // 订阅Redux的状态
 
     }
     render() { 
         return ( 
-            <div style={{margin:'10px'}}>
-                <div>
-                    <Input 
-                        placeholder={this.state.inputValue}
-                        style={{width:'250px', marginRight:'10px'}} 
-                        onChange={this.changeInputValue}
-                    />
-                    <Button type="primary" onClick={this.clickBtn}>增加</Button>
-                </div>
-                <div style={{margin:'10px',width:'300px'}}>
-                    <List
-                        bordered
-                        dataSource={this.state.list}
-                        renderItem={(item,index)=>(<List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>)}
-                    />
-                </div>
-            </div>
+            <TodoListUI 
+                inputValue={this.state.inputValue}
+                list={this.state.list}
+                changeInputValue={this.changeInputValue}
+                clickBtn={this.clickBtn}
+                deleteItem={this.deleteItem}
+            />
          );
-    }
-    changeInputValue(e){
-        const action = {
-            type:'changeInput',
-            value: e.target.value 
-        }
-        store.dispatch(action)
-    }
-    clickBtn(){
-        const action = {
-            type:"addItem",
-        }
-        store.dispatch(action)
     }
     storeChange(){
         this.setState(store.getState());
     }
+    changeInputValue(e){
+        const action = changeInputAction(e.target.value);
+        store.dispatch(action)
+    }
+    clickBtn(){
+        const action = addItemAction();
+        store.dispatch(action)
+    }
     deleteItem(index){
-        const action = {
-            type:'deleteItem',
-            index
-        }
+        const action = deleteItemAction(index);
         store.dispatch(action)
     }
 }
--- ReactDemo/demo01/src/store/reducer.js
+++ ReactDemo/demo01/src/store/reducer.js
@@ -1,3 +1,5 @@
+import { CHANGE_INPUT,ADD_ITEM,DELETE_ITEM} from './actionTypes'
+
 const defaultState = {
     inputValue:"今天要做些什么",
     list:[
@@ -9,19 +11,19 @@ const defaultState = {
 
 export default (state = defaultState, action)=>{   //方法函数
     console.log(state,action)
-    if (action.type === 'changeInput'){
+    if (action.type === CHANGE_INPUT){
         //reducer里只能接收state，不能改变state， 所以要声明一个新变量，然后用return返回去
         let newState = JSON.parse(JSON.stringify(state)); //深拷贝state
         newState.inputValue = action.value;
         return newState;
     }
-    if (action.type === 'addItem'){
+    if (action.type === ADD_ITEM){
         let newState = JSON.parse(JSON.stringify(state));
         newState.list.push(newState.inputValue);
         newState.inputValue = '';
         return newState;
     }
-    if (action.type === 'deleteItem'){
+    if (action.type === DELETE_ITEM){
         let newState = JSON.parse(JSON.stringify(state));
         newState.list.splice(action.index, 1);
         return newState;
