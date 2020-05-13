32e5f92af9fbcb6ceb0fef873db235286b2f2cf8
pingw206 <pingw206@gmail.com>
2020-05-13 22:01:50 +0800

    rename

---
+++ ReactDemo/TodoList/src/store/actionCreators.js
@@ -0,0 +1,32 @@
+import { CHANGE_INPUT,ADD_ITEM,DELETE_ITEM,GET_LIST} from './actionTypes'
+import axios from 'axios'
+
+export const changeInputAction = (value) => ({
+    type:CHANGE_INPUT,
+    value
+})
+
+export const addItemAction = () => ({
+    type:ADD_ITEM,
+})
+
+export const deleteItemAction = (index) => ({
+    type:DELETE_ITEM,
+    index
+})
+
+export const getListAction = (data) =>({
+    type:GET_LIST,
+    data
+})
+//上面的action是对象，这个action是函数
+export const getTodoList = () => {
+    return (dispatch) =>{
+        axios.get('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList')
+        .then((res)=>{
+            const data = res.data;
+            const action = getListAction(data)
+            dispatch(action)
+        })
+    }
+}
