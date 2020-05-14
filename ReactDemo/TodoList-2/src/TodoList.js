93ed4a3edf2bb4176eacff2871d2e637fb264754
pingw206 <pingw206@gmail.com>
2020-05-14 21:47:20 +0800

    未完待续

--- ReactDemo/TodoList-2/src/TodoList.js
+++ ReactDemo/TodoList-2/src/TodoList.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-
+import store from './store'
 class TodoList extends Component {
     constructor(props) {
         super(props);
+        this.state = store.getState()
     }
     render() { 
         return (
---
+++ ReactDemo/TodoList-2/src/store/reducer.js
@@ -0,0 +1,8 @@
+const defaultState = {
+    inputValue: 'write something',
+    list:[]
+}
+
+export default (state = defaultState,action) => {
+    return state;
+}
