2fe049d844e7b8fb59778ef7ce9eb0c1beed0474
pingw206 <pingw206@gmail.com>
2020-05-17 15:20:02 +0800

    后台动态获取路由

--- ReactDemo/react-router-demo-02/demo02/src/AppRouter.js
+++ ReactDemo/react-router-demo-02/demo02/src/AppRouter.js
@@ -6,21 +6,30 @@ import Video from './Pages/Video'
 import Novel from './Pages/Novel'
 
 function AppRouter() {
+    let routeCongig = [
+        {path:'/',title:'首页',exact:true, component:Index},
+        {path:'/video/',title:'视频页',exact:false,component:Video},
+        {path:'/Novel/',title:'小说页',exact:false, component:Novel}
+    ]
     return (
         <Router>
             <div className="mainDiv">
                 <div className="leftNav">
                     <h3>导航栏</h3>
                     <ul>
-                        <li><Link to="/">首页</Link></li>
-                        <li><Link to="/video">视频页</Link></li>
-                        <li><Link to="/Novel">小说页</Link></li>
+                        {
+                            routeCongig.map((item,index)=>{
+                                return(<li key={index}><Link to={item.path}>{item.title}</Link></li>)
+                            })
+                        }
                     </ul>
                 </div>
                 <div className="rightMain">
-                    <Route path="/" exact component={Index} />
-                    <Route path="/video/" component={Video} />
-                    <Route path="/Novel/" component={Novel} />
+                        {
+                            routeCongig.map((item,index)=>{
+                                return(<Route key={index} path={item.path} exact={item.exact} component={item.component} />)
+                            })
+                        }
                 </div>
             </div>
     
